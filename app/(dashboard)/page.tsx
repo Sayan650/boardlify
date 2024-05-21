8eff9887e6039d576382537904bfc0c8b4516979
Sayan650 <sayanp650@gmail.com>
2024-05-21 14:48:17 +0530

    empty states for dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,27 @@
-const Dashboard = () => {
-  return(
-      <div>
-          this is the dashboard page.
-      </div>
-  );
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/emptyOrg";
+import { BoardList } from "./_components/boardList";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favourites?: string;
+  };
 }
 
-export default  Dashboard;
+export default function DashboardPage({ searchParams }: DashboardPageProps) {
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+      {/* <EmptyOrg/> */}
+    </div>
+  );
+}
